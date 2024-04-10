025b6e8b19d15056fc382a1a2d89cf58490967df
Knowledge-JO <megamindtheincredible@gmail.com>
2024-04-10 12:23:27 +0100

    price converter

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -4,19 +4,23 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice(
+    // returns the current price of ETH in USD
+    function getETHPrice(
         AggregatorV3Interface priceFeedAddress
-    ) internal view returns (uint256) {
+    ) internal view returns (uint256 ETHPriceInUSD) {
         (, int256 answer, , , ) = priceFeedAddress.latestRoundData();
         // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+        ETHPriceInUSD = uint256(answer * 10000000000);
+        return ETHPriceInUSD;
     }
 
-    function getConversionRate(
+
+    // converts any amount of ETH passed to it to its equivalent in USD
+    function getETHConversionRate(
         uint256 ETHAmount,
         AggregatorV3Interface priceFeedAddress
     ) internal view returns (uint256) {
-        uint256 ETHPrice = getPrice(priceFeedAddress);
+        uint256 ETHPrice = getETHPrice(priceFeedAddress);
         uint256 ETHAmountInUsd = (ETHPrice * ETHAmount) /
             1000000000000000000;
 
