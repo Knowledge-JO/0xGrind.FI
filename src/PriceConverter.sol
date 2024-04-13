10445858ae44f07b17df2b9958e97b77aa7acfbc
Knowledge-JO <megamindtheincredible@gmail.com>
2024-04-13 23:01:45 +0100

    grindFI factory

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,29 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-// import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-// library PriceConverter {
-//     // returns the current price of ETH in USD
-//     function getETHPrice(
-//         AggregatorV3Interface priceFeedAddress
-//     ) internal view returns (uint256 ETHPriceInUSD) {
-//         (, int256 answer, , , ) = priceFeedAddress.latestRoundData();
-//         // ETH/USD rate in 18 digit
-//         ETHPriceInUSD = uint256(answer * 10000000000);
-//         return ETHPriceInUSD;
-//     }
+library PriceConverter {
+    // returns the current price of ETH in USD
+    function getETHPrice(
+        AggregatorV3Interface priceFeedAddress
+    ) internal view returns (uint256 ETHPriceInUSD) {
+        (, int256 answer, , , ) = priceFeedAddress.latestRoundData();
+        // ETH/USD rate in 18 digit
+        ETHPriceInUSD = uint256(answer * 10000000000);
+        return ETHPriceInUSD;
+    }
 
 
-//     // converts any amount of ETH passed to it to its equivalent in USD
-//     function getETHConversionRate(
-//         uint256 ETHAmount,
-//         AggregatorV3Interface priceFeedAddress
-//     ) internal view returns (uint256) {
-//         uint256 ETHPrice = getETHPrice(priceFeedAddress);
-//         uint256 ETHAmountInUsd = (ETHPrice * ETHAmount) /
-//             1000000000000000000;
+    // converts any amount of ETH passed to it to its equivalent in USD
+    function getETHConversionRate(
+        uint256 ETHAmount,
+        AggregatorV3Interface priceFeedAddress
+    ) internal view returns (uint256) {
+        uint256 ETHPrice = getETHPrice(priceFeedAddress);
+        uint256 ETHAmountInUsd = (ETHPrice * ETHAmount) /
+            1000000000000000000;
 
-//         return ETHAmountInUsd;
-//     }
-// }
+        return ETHAmountInUsd;
+    }
+}
